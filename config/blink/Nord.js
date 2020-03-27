d993d7a88d766294c9d3cec4b8b2c77f0f271b49
Nathaniel Watts <1141717+thewatts@users.noreply.github.com>
2020-03-27 12:10:04 -0500

    Add Nord theme for Blink shell

---
+++ config/blink/Nord.js
@@ -0,0 +1,21 @@
+t.prefs_.set('color-palette-overrides', [
+  "#3B4252",
+  "#BE6169",
+  "#A4BE8B",
+  "#EBCB8B",
+  "#81A1C1",
+  "#B58EAD",
+  "#89BFD0",
+  "#E5E9F1",
+  "#4C566A",
+  "#BF606B",
+  "#A3BE8B",
+  "#EBCB8B",
+  "#81A1C1",
+  "#B48EAD",
+  "#8FBCBC",
+  "#EBEFF3",
+]);
+t.prefs_.set('foreground-color', "#D9DEEA");
+t.prefs_.set('background-color', "#2C3540");
+t.prefs_.set('cursor-color', 'rgba(216, 222, 233, 0.5)');
